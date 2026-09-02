537d1d7a32d3b098dff655b2b54ac3d74f42a9d7
Moilab <moilab.web@gmail.com>
2026-09-02 18:17:03 +0200

    feat(): create user test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,6 +4,9 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto';
+import * as bcrypt from 'bcrypt';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -40,5 +43,57 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(authService).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    const createUserDto: CreateUserDto = {
+      email: 'test@test.com',
+      password: 'Test1234',
+      fullName: 'Test User',
+    };
+
+    const user = {
+      id: '1',
+      email: createUserDto.email,
+      fullName: createUserDto.fullName,
+      isActive: true,
+      roles: ['user'],
+    };
+
+    jest.spyOn(userRepository, 'create').mockReturnValue(user as User);
+    jest.spyOn(bcrypt, 'hashSync').mockReturnValue('mocked-hashed-password');
+
+    const result = await authService.create(createUserDto);
+
+    expect(bcrypt.hashSync).toHaveBeenCalledWith(createUserDto.password, 10);
+    expect(result).toEqual({
+      user: {
+        id: '1',
+        email: 'test@test.com',
+        fullName: 'Test User',
+        isActive: true,
+        roles: ['user'],
+      },
+      token: 'mocked-jwt-token',
+    });
+  });
+
+  it('should throw an error if the user already exists', async () => {
+    const createUserDto: CreateUserDto = {
+      email: 'test@test.com',
+      password: 'Test1234',
+      fullName: 'Test User',
+    };
+
+    jest
+      .spyOn(userRepository, 'save')
+      .mockRejectedValue({ code: '23505', detail: 'Email already exists' });
+
+    await expect(authService.create(createUserDto)).rejects.toThrow(
+      BadRequestException,
+    );
+    await expect(authService.create(createUserDto)).rejects.toThrow(
+      'Email already exists',
+    );
   });
 });
