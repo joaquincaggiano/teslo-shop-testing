b86b4198b3e3f2eb8f50fe6be0128f2d39f91220
Moilab <moilab.web@gmail.com>
2026-09-02 17:52:23 +0200

    feat(): auth service is defined test

---
+++ src/auth/auth.service.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { Repository } from 'typeorm';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let userRepository: Repository<User>;
+
+  beforeEach(async () => {
+    const mockUserRepository = {
+      create: jest.fn(),
+      save: jest.fn(),
+      findOne: jest.fn(),
+    };
+
+    const mockJwtService = {
+      sign: jest.fn().mockReturnValue('mocked-jwt-token'),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+      ],
+    }).compile();
+
+    authService = module.get<AuthService>(AuthService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+});
